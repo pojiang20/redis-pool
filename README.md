f1c89d300e5b8b74f3d4f2db4b89f42effc9e9c5
pojiang20 <1021465760@qq.com>
2022-12-12 15:49:17 +0800

    redis-pool简单使用

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # redis-pool
 学习redis池化
+
+main.go中执行的结果如下，pool的初始化配置是maxIdle=2,maxActive=3。
+最开始从pool中获取5个连接，由于设置了maxActive=3因此，因此`idle conn: 0,active conn: 3`
+再将获取所有的连接关闭，由于设置了maxIdle=2，`idle conn: 2,active conn: 2`，此时一定有idle conn==active conn。
+再从pool中获取3个连接，此时`idle conn`用完，`active conn`变为3.
+再次将所有连接关闭，恢复到`idle conn: 2,active conn: 2`。
+```text
+connGroup have 5 conn
+idle conn: 0,active conn: 3
+close 5 conn
+idle conn: 2,active conn: 2
+idle conn: 0,active conn: 3
+close 3 conn
+idle conn: 2,active conn: 2
+
+```
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	redigo "github.com/gomodule/redigo/redis"
+	"time"
+)
+
+func main() {
+	var addr = "127.0.0.1:6379"
+	var password = ""
+
+	pool := PoolInitRedis(addr, password)
+	connGroup := make([]redigo.Conn, 0, 5)
+	for i := 0; i < 5; i++ {
+		connGroup = append(connGroup, pool.Get())
+	}
+	fmt.Printf("connGroup have %d conn\n", len(connGroup))
+	fmt.Printf("idle conn: %d,active conn: %d\n", pool.IdleCount(), pool.ActiveCount())
+
+	for _, v := range connGroup {
+		v.Close()
+	}
+	fmt.Println("close 5 conn")
+	fmt.Printf("idle conn: %d,active conn: %d\n", pool.IdleCount(), pool.ActiveCount())
+
+	//下次是怎么取出来的？？
+	b1 := pool.Get()
+	b2 := pool.Get()
+	b3 := pool.Get()
+	fmt.Printf("idle conn: %d,active conn: %d\n", pool.IdleCount(), pool.ActiveCount())
+	b1.Close()
+	b2.Close()
+	b3.Close()
+	fmt.Println("close 3 conn")
+	fmt.Printf("idle conn: %d,active conn: %d\n", pool.IdleCount(), pool.ActiveCount())
+}
+
+// redis pool
+func PoolInitRedis(server string, password string) *redigo.Pool {
+	return &redigo.Pool{
+		MaxIdle:     2, //空闲数
+		IdleTimeout: 240 * time.Second,
+		MaxActive:   3, //最大数
+		Dial: func() (redigo.Conn, error) {
+			c, err := redigo.Dial("tcp", server)
+			if err != nil {
+				return nil, err
+			}
+			if password != "" {
+				if _, err := c.Do("AUTH", password); err != nil {
+					c.Close()
+					return nil, err
+				}
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redigo.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+func printAvailableConn(connGroup []redigo.Conn) {
+	cnt := 0
+	for _, v := range connGroup {
+		if v.Err() == nil {
+			cnt++
+		}
+	}
+	fmt.Printf("there is %d available conn\n", cnt)
+}
